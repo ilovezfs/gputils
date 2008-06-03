89f7bf2702a6e721e686fe15523c404ed5cd3a91
merc64 <merc64@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2008-06-03 07:56:27 +0000

    Fixed line number problems with CODE_PACK directive (bug #1972099)

---
+++ trunk/gputils/gpasm/testsuite/gpasm.project/objasm/code_pack.asm
@@ -0,0 +1,13 @@
+    radix dec
+    list p=18f4550
+    #include <p18f4550.inc>
+
+    code_pack
+    db 1,2,3
+    db 4,5,6
+    db 1,2,3,4,5,6,7,8
+    movlw 2
+sec2    code
+    movlw 1
+    db 1,2,3
+    end
