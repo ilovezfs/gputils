a3ee3c41f2a645f5ceaf4c0172f893bb86452e61
kohtala <kohtala@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2009-03-12 21:42:17 +0000

    Add LIST M=??? support

--- gputils/libgputils/gpprocessor.c
+++ gputils/libgputils/gpprocessor.c
@@ -27,7 +27,7 @@ Boston, MA 02111-1307, USA.  */
    alphabetically. */
 
 static struct px pics[] = {
-  { PROC_CLASS_EEPROM8, "__EEPROM8",  { "eeprom8",     "eeprom8",   "eeprom8"  }, 0x1fff, 0, 0, -1,       { -1, -1 },            { -1, -1 },             NULL           },
+  { PROC_CLASS_EEPROM8, "__EEPROM8",  { "eeprom8",     "eeprom8",   "eeprom8"  }, 0x1fff, 0, 0, 0x7f,     { -1, -1 },            { -1, -1 },             NULL           },
   { PROC_CLASS_GENERIC, "__GEN",      { "generic",     "gen",       "unknown"  }, 0x0000, 0, 0, -1,       { -1, -1 },            { -1, -1 },             NULL           },
   { PROC_CLASS_PIC12,   "__HCS1365",  { "hcs1365",     "hcs1365",   "hcs1365"  }, 0xf365, 2, 4, 0x3ff,    { -1, -1 },            { 0x7FF, 0x7FF },       "hcs1365.lkr"  },
   { PROC_CLASS_PIC12,   "__HCS1370",  { "hcs1370",     "hcs1370",   "hcs1370"  }, 0xf370, 2, 4, 0x3ff,    { -1, -1 },            { 0x7FF, 0x7FF },       "hcs1370.lkr"  },
