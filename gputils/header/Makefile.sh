c848143ff7e3e183ef1b8d38774e9c8179475ce9
borutr <borutr@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2012-02-25 12:12:54 +0000

    * header/Makefile.sh: let replaced with expr * header/Makefile.am, header/Makefile.in: regenarated;   thanks to Raphael Neider.

--- gputils/header/Makefile.sh
+++ gputils/header/Makefile.sh
@@ -17,17 +17,17 @@ rm -f header_check
 gcc -Wall -pedantic -g -O2 -I../include -o header_check header_check.c
 
 # count the number of items in the list
-let count=0
+count=0
 for x in *.inc
   do
-    let count=count+1
+    count=$(expr $count + 1)
   done
 
 # output the file list
-let number=0
+number=0
 for x in *.inc
   do
-    let number=number+1
+    number=$(expr $number + 1)
     echo "testing $x"
     ./header_check $x
     if [ $number -eq $count ]; then
