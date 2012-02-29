7478ac178d8dab6f5b586dc6a2115a6fc947fe74
borutr <borutr@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2012-02-29 09:42:27 +0000

    * get_cl_revision.sh:   added utility to extract source control revision number from ChangeLog * Makefile.am, Makefile.in,   gputils/Makefile.am, gputils/Makefile.in,   gplink/Makefile.am, gplink/Makefile.in,   gpasm/Makefile.am, gpasm/Makefile.in: build *.h from *.h.in * gpasm/gpasm.h, gplink/gplink.h, gputils/gpdasm.h, gputils/gplib.h,   gputils/gpstrip.h, gputils/gpvc.h, gputils/gpvo.h:   renamed to *.h.in, added source control revision number to --version

---
+++ gputils/get_cl_revision.sh
@@ -0,0 +1,44 @@
+# get_cl_revision.sh
+#
+# Extraxts the source control revision number from the ChangeLog file.
+# the $Revision$ keyword should be in the last line of the file
+
+VERSION=1.0
+AS_STRING=0
+
+usage ()
+{
+  echo "usage: $0 [options] <ChangeLog_file_name>"
+  echo "options:"
+  echo "  -s, --string          return the revision number as double quoted string"
+  echo "  -V, --version         output version information and exit"
+  echo "  -h, --help            display this help and exit"
+  echo "arguments:"
+  echo "  <ChangeLog_file_name>  ChangeLog file name"
+  exit 0;
+}
+
+while true
+do
+  case "$1" in
+    -s|--string) AS_STRING=1; shift;;
+    -h|--help) usage; exit 0;;
+    -V|--version) echo $(basename $0) $VERSION; exit 0;;
+    --) shift; break;;
+    -*|--*) echo "unknown option $1"; usage; exit 1;;
+    ''|*) break;;
+  esac
+done
+
+if [ "$#" != 1 ]
+then
+  echo "too many arguments"; usage; exit 1;
+else
+  REV=$(expr "$(tail -n 1 $1)" : '\$Revision: *\([0-9]*\) *\$') || REV=0
+  if [ "$AS_STRING" != 0 ]
+  then
+    echo "\"$REV\""
+  else
+    echo $REV
+  fi
+fi
