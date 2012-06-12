21cedd69fffa8306380f35cb827c12365c4803dd
borutr <borutr@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2012-06-12 09:39:32 +0000

    * gputils/block.c, gputils/block.h, gputils/gpvc.c:   read_directory() returns pointer to DirBlockInfo

--- gputils/gputils/block.c
+++ gputils/gputils/block.c
@@ -36,27 +36,27 @@ void directory_block(void)
 
   block = main_dir->dir.block;
 
-  printf("directory block \n");
+  printf("directory block\n");
 
   printf("COD file version  %d\n",
          gp_getl16(&block[COD_DIR_CODTYPE]));
   printf("Source file       %s\n",
          &block[COD_DIR_SOURCE]);
   printf("Date              %s\n",
-         substr(temp_buf, sizeof(temp_buf), &block[COD_DIR_DATE],7));
+         substr(temp_buf, sizeof (temp_buf), &block[COD_DIR_DATE],7));
 
   time = gp_getl16(&block[COD_DIR_TIME]);
 
   printf("Time              %02d:%02d\n", time / 100, time % 100);
   printf("Compiler version  %s\n",
-         substr(temp_buf, sizeof(temp_buf), &block[COD_DIR_VERSION],19));
+         substr(temp_buf, sizeof (temp_buf), &block[COD_DIR_VERSION],19));
   printf("Compiler          %s\n",
-         substr(temp_buf, sizeof(temp_buf), &block[COD_DIR_COMPILER],12));
+         substr(temp_buf, sizeof (temp_buf), &block[COD_DIR_COMPILER],12));
   printf("Notice            %s\n",
-         substr(temp_buf, sizeof(temp_buf), &block[COD_DIR_NOTICE],64));
+         substr(temp_buf, sizeof (temp_buf), &block[COD_DIR_NOTICE],64));
 
   processor_name = substr(temp_buf,
-                          sizeof(temp_buf),
+                          sizeof (temp_buf),
                           &block[COD_DIR_PROCESSOR],
                           8);
   printf("Processor         %s\n", processor_name);
@@ -72,88 +72,69 @@ void directory_block(void)
 
   printf("Short symbol table start block:  0x%04x  end block:  0x%04x\n",
          gp_getl16(&block[COD_DIR_SYMTAB]),
-         gp_getl16(&block[COD_DIR_SYMTAB+2]));
+         gp_getl16(&block[COD_DIR_SYMTAB + 2]));
   printf("Long symbol table start block:   0x%04x  end block:  0x%04x\n",
          gp_getl16(&block[COD_DIR_LSYMTAB]),
-         gp_getl16(&block[COD_DIR_LSYMTAB+2]));
+         gp_getl16(&block[COD_DIR_LSYMTAB + 2]));
   printf("File name table start block:     0x%04x  end block:  0x%04x\n",
          gp_getl16(&block[COD_DIR_NAMTAB]),
-         gp_getl16(&block[COD_DIR_NAMTAB+2]));
+         gp_getl16(&block[COD_DIR_NAMTAB + 2]));
   printf("Source info table start block:   0x%04x  end block:  0x%04x\n",
          gp_getl16(&block[COD_DIR_LSTTAB]),
-         gp_getl16(&block[COD_DIR_LSTTAB+2]));
+         gp_getl16(&block[COD_DIR_LSTTAB + 2]));
   printf("Rom table start block:           0x%04x  end block:  0x%04x\n",
          gp_getl16(&block[COD_DIR_MEMMAP]),
-         gp_getl16(&block[COD_DIR_MEMMAP+2]));
+         gp_getl16(&block[COD_DIR_MEMMAP + 2]));
   printf("Local scope table start block:   0x%04x  end block:  0x%04x\n",
          gp_getl16(&block[COD_DIR_LOCALVAR]),
-         gp_getl16(&block[COD_DIR_LOCALVAR+2]));
+         gp_getl16(&block[COD_DIR_LOCALVAR + 2]));
   printf("Debug messages start block:      0x%04x  end block:  0x%04x\n",
          gp_getl16(&block[COD_DIR_MESSTAB]),
-         gp_getl16(&block[COD_DIR_MESSTAB+2]));
+         gp_getl16(&block[COD_DIR_MESSTAB + 2]));
 
   printf("\nNext directory block");
-  if(gp_getl16(&block[COD_DIR_NEXTDIR]))
+  if (gp_getl16(&block[COD_DIR_NEXTDIR]))
     printf(":  %d\n",gp_getl16(&block[COD_DIR_NEXTDIR]));
   else
     printf(" is empty\n");
-
-  /*
-    Uncomment this section to get the address offsets within the directory structure
-    NOTE: the offsets are compiler dependent. For gcc, use the -fpack-struct
-    option to pack the structures with no gaps.
-
-    printf("notice_strlen address %d\n",ptr_offset((char *)&dir->notice_strlen,block) );
-    printf("notice address %d\n",ptr_offset((char *)&dir->notice,block) );
-    printf("symtab address %d\n",ptr_offset((char *)&dir->symtab,block) );
-    printf("processor address %d\n",ptr_offset((char *)&dir->processor,block) );
-    printf("AddrSize address %d\n",ptr_offset((char *)&dir->AddrSize,block) );
-    printf("HighAddr address %d\n",ptr_offset((char *)&dir->HighAddr,block) );
-    printf("NextDir address %d\n",ptr_offset((char *)&dir->NextDir,block) );
-    printf("memmap address %d\n",ptr_offset((char *)&dir->MemMapOFS,block) );
-    printf("lsymtab address %d\n",ptr_offset((char *)&dir->Lsymtab,block) );
-  */
-
-
 }
 
-void read_block(unsigned char * block, int block_number)
+void
+read_block(unsigned char * block, int block_number)
 {
-
   fseek(codefile, block_number * COD_BLOCK_SIZE, SEEK_SET);
   fread(block, COD_BLOCK_SIZE, 1, codefile);
 }
 
-void create_block(Block *b)
+void
+create_block(Block *b)
 {
-
   assert(b != NULL);
 
   b->block = malloc(COD_BLOCK_SIZE);
   gp_cod_clear(b);
-
 }
 
-void read_directory(void)
+DirBlockInfo *
+read_directory(void)
 {
-  DirBlockInfo *dbi;
-
-  create_block(&main_dir->dir);
-  read_block(main_dir->dir.block, 0);
-
-  dbi = main_dir;
+  DirBlockInfo *dbi = NULL, *start = NULL;
+  int next_dir_block = 0;
 
   do {
-    int next_dir_block = gp_getl16(&dbi->dir.block[COD_DIR_NEXTDIR]);
-
-    if(next_dir_block) {
-      dbi->next_dir_block_info = (DirBlockInfo *)malloc(sizeof(DirBlockInfo));
-      dbi = dbi->next_dir_block_info;
-      create_block(&dbi->dir);
-      read_block(dbi->dir.block, next_dir_block);
-    } else {
-      dbi->next_dir_block_info = NULL;
-      return;
+    DirBlockInfo *p = (DirBlockInfo *)malloc(sizeof (DirBlockInfo));
+    assert(p);
+    if (NULL == dbi)
+      start = dbi = p;
+    else {
+      dbi->next_dir_block_info = p;
+      dbi = p;
     }
-  } while(1);
+    create_block(&dbi->dir);
+    read_block(dbi->dir.block, next_dir_block);
+  } while (0 != (next_dir_block = gp_getl16(&dbi->dir.block[COD_DIR_NEXTDIR])));
+
+  dbi->next_dir_block_info = NULL;
+
+  return start;
 }
