2ce0e13b71fe28917b9732c5c7ef02d38389321f
borutr <borutr@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2012-09-09 13:18:14 +0000

    * scripts/build/mingw/do_setup.sh:   detect gputils version from configure.ac, cygwin gcc-4

--- scripts/build/mingw/do_setup.sh
+++ scripts/build/mingw/do_setup.sh
@@ -1,8 +1,11 @@
+#!/bin/sh
+
 if test -n "$1"
 then
   srcfilename=$1
 else
-  srcfilename=gputils-0.13.7.tar.gz
+  ver=$(sed -n 's/AC_INIT(\[gputils\],[ \t]*\[\([^\]*\)\],[ \t]*\[[^\]*\])$/\1/p' ../../../gputils/configure.ac)
+  srcfilename=gputils-${ver}.tar.gz
 fi
 
 if test -n "$2"
@@ -19,6 +22,9 @@ else
   email=borut.razem@siol.net
 fi
 
-CC="gcc -mno-cygwin" ./make-mingw --email=$email --name="$packager" --source=$srcfilename --save-temps
+# gcc-3
+#CC="gcc -mno-cygwin" ./make-mingw --email=$email --name="$packager" --source=$srcfilename --save-temps
+#gcc-4
+CC="i686-pc-mingw32-gcc" ./make-mingw --email=$email --name="$packager" --source=$srcfilename --save-temps
 # on mingw32:
 #CC=gcc ./make-mingw --email=$email --name="$packager" --source=$srcfilename --save-temps
