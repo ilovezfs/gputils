0269e6db54fb5ee37662088c1213eeacdd145ee5
borutr <borutr@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2013-03-08 18:09:14 +0000

    - support for PROC_CLASS_PIC12E

--- gputils/libgputils/gpopcode.c
+++ gputils/libgputils/gpopcode.c
@@ -118,7 +118,7 @@ const struct insn op_sx[] = {
   { "sleep",  0xfff, 0x003,     INSN_CLASS_IMPLICIT     },
   { "subwf",  0xfc0, 0x080,     INSN_CLASS_OPWF5        },
   { "swapf",  0xfc0, 0x380,     INSN_CLASS_OPWF5        },
-  { "tris",   0xff8, 0x000,     INSN_CLASS_OPF5         },
+  { "tris",   0xff8, 0x000,     INSN_CLASS_OPF5         }, /* FIXME: this should probably be INSN_CLASS_OPF3 as for 12c5xx? */
   { "xorlw",  0xf00, 0xf00,     INSN_CLASS_LIT8         },
   { "xorwf",  0xfc0, 0x180,     INSN_CLASS_OPWF5        }
 };
