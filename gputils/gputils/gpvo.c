99ed8f2ddbcdd286dadc7ee38aa8d4e275dd132c
borutr <borutr@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2013-04-01 17:54:12 +0000

    * gputils/gpvo.c: fixed printing 1 byte config data

--- gputils/gputils/gpvo.c
+++ gputils/gputils/gpvo.c
@@ -144,6 +144,7 @@ void print_data(proc_class_t class, gp_section_type *section)
   while (1) {
     if ((section->flags & STYP_TEXT) && class->find_insn != NULL) {
       unsigned short memory;
+
       if (!class->i_memory_get(section->data, address, &memory))
         break;
       num_words = gp_disassemble(section->data,
@@ -157,18 +158,30 @@ void print_data(proc_class_t class, gp_section_type *section)
         printf("%06x:  %04x\n", gp_processor_byte_to_org(class, address + 2), memory);
       }
       address += 2 * num_words;
-    } else if (section->flags & STYP_DATA_ROM || class == PROC_CLASS_EEPROM16) {
-      unsigned short memory;
-      if (!class->i_memory_get(section->data, address, &memory))
+    }
+    else if (section->flags & STYP_DATA_ROM || class == PROC_CLASS_EEPROM16) {
+      unsigned short word;
+
+      if (class->i_memory_get(section->data, address, &word)) {
+        printf("%06x:  %04x\n", gp_processor_byte_to_org(class, address), word);
+        address += 2;
+      }
+      else {
+        unsigned char byte;
+
+        if (b_memory_get(section->data, address, &byte)) {
+          printf("%06x:  %02x\n", gp_processor_byte_to_org(class, address), byte);
+        }
         break;
-      printf("%06x:  %04x\n", gp_processor_byte_to_org(class, address), memory);
-      address += 2;
-    } else {
+      }
+    }
+    else {
       /* STYP_DATA or STYP_ACTREC, or EEPROM8 */
-      unsigned char b;
-      if (!b_memory_get(section->data, address, &b))
+      unsigned char byte;
+
+      if (!b_memory_get(section->data, address, &byte))
         break;
-      printf("%06x:  %02x\n", address, b);
+      printf("%06x:  %02x\n", address, byte);
       ++address;
     }
   }
