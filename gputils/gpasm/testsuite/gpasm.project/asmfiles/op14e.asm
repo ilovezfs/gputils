38d7762806f26756b7b93d8f70e472463b8dd57f
borutr <borutr@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2013-08-02 10:01:03 +0000

    * gpasm/testsuite/gpasm.project/asmfiles/op14e.asm,   gpasm/testsuite/gpasm.project/listfiles/op14e.lst,   gpasm/testsuite/gpasm.project/hexfiles/op14e.hex,   gpasm/testsuite/gpasm.project/objasm/op14e.asm,   gpasm/testsuite/gpasm.project/objlstfiles/op14e.lst,   gpasm/testsuite/gpasm.project/objfiles/op14e.o:   added gpasm regression tests for bug   #268 PIC14e: HIGH directive not setting bit-7

--- gputils/gpasm/testsuite/gpasm.project/asmfiles/op14e.asm
+++ gputils/gpasm/testsuite/gpasm.project/asmfiles/op14e.asm
@@ -1,178 +1,178 @@
         processor 16f1933
-        radix dec
+        radix   dec
 
-FSR0    equ H'0004'
-FSR1    equ H'0006'
+FSR0    equ     H'0004'
+FSR1    equ     H'0006'
 
 ; INSN_CLASS_MOVINDF
-        moviw ++FSR0
-        moviw --FSR0
-        moviw FSR0++
-        moviw FSR0--
+        moviw   ++FSR0
+        moviw   --FSR0
+        moviw   FSR0++
+        moviw   FSR0--
 
-        moviw ++FSR1
-        moviw --FSR1
-        moviw FSR1++
-        moviw FSR1--
+        moviw   ++FSR1
+        moviw   --FSR1
+        moviw   FSR1++
+        moviw   FSR1--
 
-        movwi ++FSR0
-        movwi --FSR0
-        movwi FSR0++
-        movwi FSR0--
+        movwi   ++FSR0
+        movwi   --FSR0
+        movwi   FSR0++
+        movwi   FSR0--
 
-        movwi ++FSR1
-        movwi --FSR1
-        movwi FSR1++
-        movwi FSR1--
+        movwi   ++FSR1
+        movwi   --FSR1
+        movwi   FSR1++
+        movwi   FSR1--
 
-moviwx  macro val
-        moviw val[FSR0]
-        moviw val[FSR1]
+moviwx  macro   val
+        moviw   val[FSR0]
+        moviw   val[FSR1]
         endm
 
-        moviwx 0
-        moviwx 1
-        moviwx 2
-        moviwx 3
-        moviwx 4
-        moviwx 5
-        moviwx 6
-        moviwx 7
-        moviwx 8
-        moviwx 9
-        moviwx 10
-        moviwx 11
-        moviwx 12
-        moviwx 13
-        moviwx 14
-        moviwx 15
-        moviwx 16
-        moviwx 17
-        moviwx 18
-        moviwx 19
-        moviwx 20
-        moviwx 21
-        moviwx 22
-        moviwx 23
-        moviwx 24
-        moviwx 25
-        moviwx 26
-        moviwx 27
-        moviwx 28
-        moviwx 29
-        moviwx 30
-        moviwx 31
+        moviwx  0
+        moviwx  1
+        moviwx  2
+        moviwx  3
+        moviwx  4
+        moviwx  5
+        moviwx  6
+        moviwx  7
+        moviwx  8
+        moviwx  9
+        moviwx  10
+        moviwx  11
+        moviwx  12
+        moviwx  13
+        moviwx  14
+        moviwx  15
+        moviwx  16
+        moviwx  17
+        moviwx  18
+        moviwx  19
+        moviwx  20
+        moviwx  21
+        moviwx  22
+        moviwx  23
+        moviwx  24
+        moviwx  25
+        moviwx  26
+        moviwx  27
+        moviwx  28
+        moviwx  29
+        moviwx  30
+        moviwx  31
 
-        moviwx -1
-        moviwx -2
-        moviwx -3
-        moviwx -4
-        moviwx -5
-        moviwx -6
-        moviwx -7
-        moviwx -8
-        moviwx -9
-        moviwx -10
-        moviwx -11
-        moviwx -12
-        moviwx -13
-        moviwx -14
-        moviwx -15
-        moviwx -16
-        moviwx -17
-        moviwx -18
-        moviwx -19
-        moviwx -20
-        moviwx -21
-        moviwx -22
-        moviwx -23
-        moviwx -24
-        moviwx -25
-        moviwx -26
-        moviwx -27
-        moviwx -28
-        moviwx -29
-        moviwx -30
-        moviwx -31
-        moviwx -32
+        moviwx  -1
+        moviwx  -2
+        moviwx  -3
+        moviwx  -4
+        moviwx  -5
+        moviwx  -6
+        moviwx  -7
+        moviwx  -8
+        moviwx  -9
+        moviwx  -10
+        moviwx  -11
+        moviwx  -12
+        moviwx  -13
+        moviwx  -14
+        moviwx  -15
+        moviwx  -16
+        moviwx  -17
+        moviwx  -18
+        moviwx  -19
+        moviwx  -20
+        moviwx  -21
+        moviwx  -22
+        moviwx  -23
+        moviwx  -24
+        moviwx  -25
+        moviwx  -26
+        moviwx  -27
+        moviwx  -28
+        moviwx  -29
+        moviwx  -30
+        moviwx  -31
+        moviwx  -32
 
-movwix  macro val
-        movwi val[FSR0]
-        movwi val[FSR1]
+movwix  macro   val
+        movwi   val[FSR0]
+        movwi   val[FSR1]
         endm
 
-        movwix 0
-        movwix 1
-        movwix 2
-        movwix 3
-        movwix 3
-        movwix 4
-        movwix 5
-        movwix 6
-        movwix 7
-        movwix 8
-        movwix 9
-        movwix 10
-        movwix 11
-        movwix 12
-        movwix 13
-        movwix 14
-        movwix 15
-        movwix 16
-        movwix 17
-        movwix 18
-        movwix 19
-        movwix 20
-        movwix 21
-        movwix 22
-        movwix 23
-        movwix 24
-        movwix 25
-        movwix 26
-        movwix 27
-        movwix 28
-        movwix 29
-        movwix 30
-        movwix 31
+        movwix  0
+        movwix  1
+        movwix  2
+        movwix  3
+        movwix  3
+        movwix  4
+        movwix  5
+        movwix  6
+        movwix  7
+        movwix  8
+        movwix  9
+        movwix  10
+        movwix  11
+        movwix  12
+        movwix  13
+        movwix  14
+        movwix  15
+        movwix  16
+        movwix  17
+        movwix  18
+        movwix  19
+        movwix  20
+        movwix  21
+        movwix  22
+        movwix  23
+        movwix  24
+        movwix  25
+        movwix  26
+        movwix  27
+        movwix  28
+        movwix  29
+        movwix  30
+        movwix  31
 
-        movwix -1
-        movwix -2
-        movwix -3
-        movwix -3
-        movwix -4
-        movwix -5
-        movwix -6
-        movwix -7
-        movwix -8
-        movwix -9
-        movwix -10
-        movwix -11
-        movwix -12
-        movwix -13
-        movwix -14
-        movwix -15
-        movwix -16
-        movwix -17
-        movwix -18
-        movwix -19
-        movwix -20
-        movwix -21
-        movwix -22
-        movwix -23
-        movwix -24
-        movwix -25
-        movwix -26
-        movwix -27
-        movwix -28
-        movwix -29
-        movwix -30
-        movwix -31
-        movwix -32
+        movwix  -1
+        movwix  -2
+        movwix  -3
+        movwix  -3
+        movwix  -4
+        movwix  -5
+        movwix  -6
+        movwix  -7
+        movwix  -8
+        movwix  -9
+        movwix  -10
+        movwix  -11
+        movwix  -12
+        movwix  -13
+        movwix  -14
+        movwix  -15
+        movwix  -16
+        movwix  -17
+        movwix  -18
+        movwix  -19
+        movwix  -20
+        movwix  -21
+        movwix  -22
+        movwix  -23
+        movwix  -24
+        movwix  -25
+        movwix  -26
+        movwix  -27
+        movwix  -28
+        movwix  -29
+        movwix  -30
+        movwix  -31
+        movwix  -32
 
 ; INSN_CLASS_LITFSR
-addfsrx macro val
-        addfsr FSR0,val
-        addfsr FSR1,val
+addfsrx macro   val
+        addfsr  FSR0,val
+        addfsr  FSR1,val
         endm
 
         addfsrx 0
@@ -246,45 +246,45 @@ addfsrx macro val
 MAXROM = H'07FF'
 MAXRAM = H'007F'
 
-        addwfc 0
-        addwfc 0,1
-        addwfc 0,F
-        addwfc 0,0
-        addwfc 0,W
-        addwfc MAXRAM
+        addwfc  0
+        addwfc  0,1
+        addwfc  0,F
+        addwfc  0,0
+        addwfc  0,W
+        addwfc  MAXRAM
 
-        asrf 0
-        asrf 0,1
-        asrf 0,F
-        asrf 0,0
-        asrf 0,W
-        asrf MAXRAM
+        asrf    0
+        asrf    0,1
+        asrf    0,F
+        asrf    0,0
+        asrf    0,W
+        asrf    MAXRAM
 
-        lslf 0
-        lslf 0,1
-        lslf 0,F
-        lslf 0,0
-        lslf 0,W
-        lslf MAXRAM
+        lslf    0
+        lslf    0,1
+        lslf    0,F
+        lslf    0,0
+        lslf    0,W
+        lslf    MAXRAM
 
-        lsrf 0
-        lsrf 0,1
-        lsrf 0,F
-        lsrf 0,0
-        lsrf 0,W
-        lsrf MAXRAM
+        lsrf    0
+        lsrf    0,1
+        lsrf    0,F
+        lsrf    0,0
+        lsrf    0,W
+        lsrf    MAXRAM
 
-        subwfb 0
-        subwfb 0,1
-        subwfb 0,F
-        subwfb 0,0
-        subwfb 0,W
-        subwfb MAXRAM
+        subwfb  0
+        subwfb  0,1
+        subwfb  0,F
+        subwfb  0,0
+        subwfb  0,W
+        subwfb  MAXRAM
 
 ; INSN_CLASS_RBRA9
 
-        bra $+256
-        bra $-255
+        bra     $+256
+        bra     $-255
 
 ; INSN_CLASS_IMPLICIT
         brw
@@ -292,11 +292,20 @@ MAXRAM = H'007F'
         reset
 
 ; INSN_CLASS_LIT5
-        movlb 0
-        movlb 31
+        movlb   0
+        movlb   31
 
 ; INSN_CLASS_LIT7
-        movlp 0
-        movlp 127
+        movlp   0
+        movlp   127
 
+; HIGH
+        cblock  0x10
+        table_data: 1
+        endc
+
+        data    HIGH table_code
+        data    HIGH table_data
+
+table_code
         end
--- gputils/gpasm/testsuite/gpasm.project/objasm/op14e.asm
+++ gputils/gpasm/testsuite/gpasm.project/objasm/op14e.asm
@@ -1,79 +1,87 @@
 ; This file is used to test relocatable objects with the enhanced 14 bit
 ; instruction set. Some of the expressions are possible, but not very useful.
 
-    list p=12f1822,r=dec
-    extern file, address
+        list p=12f1822, r=dec
+        extern file, address
 
-FSR0 equ H'0004'
-FSR1 equ H'0006'
+FSR0    equ     H'0004'
+FSR1    equ     H'0006'
 
-    code
+        code
 
-    addfsr FSR0,-32
-    addfsr FSR0,31
-    addfsr FSR1,-32
-    addfsr FSR1,31
-    addlw file
-    addwf file,1
-    addwfc file,1
-    andlw file
-    andwf file,1
-    asrf file,1
-    banksel file
-    bcf file,7
-    bra address
-    brw
-    bsf file,7
-    btfsc file,1
-    btfss file,1
-    call address
-    callw
-    clrf file
-    clrw
-    clrwdt
-    comf file,1
-    decf file,1
-    decfsz file,1
-    pagesel address
-    goto address
-    incf file,1
-    incfsz file,1
-    iorlw file
-    iorwf file,1
-    lslf file,1
-    lsrf file,1
+        addfsr  FSR0,-32
+        addfsr  FSR0,31
+        addfsr  FSR1,-32
+        addfsr  FSR1,31
+        addlw   file
+        addwf   file,1
+        addwfc  file,1
+        andlw   file
+        andwf   file,1
+        asrf    file,1
+        banksel file
+        bcf     file,7
+        bra     address
+        brw
+        bsf     file,7
+        btfsc   file,1
+        btfss   file,1
+        call    address
+        callw
+        clrf    file
+        clrw
+        clrwdt
+        comf   file,1
+        decf   file,1
+        decfsz file,1
+        pagesel address
+        goto   address
+        incf   file,1
+        incfsz file,1
+        iorlw  file
+        iorwf  file,1
+        lslf   file,1
+        lsrf   file,1
 
-    moviw ++FSR0
-    moviw --FSR0
-    moviw FSR0++
-    moviw FSR0--
+        moviw  ++FSR0
+        moviw  --FSR0
+        moviw  FSR0++
+        moviw  FSR0--
 
-    movf  file,1
-    movlb file
-    movlp file
-    movlw file
-    movwf file
+        movf   file,1
+        movlb  file
+        movlp  file
+        movlw  file
+        movwf  file
 
-    movwi ++FSR0
-    movwi --FSR0
-    movwi FSR0++
-    movwi FSR0--
+        movwi  ++FSR0
+        movwi  --FSR0
+        movwi  FSR0++
+        movwi  FSR0--
 
-    nop
-    option
-    reset
-    retfie
-    retlw file
-    return
-    rlf file,1
-    rrf file,1
-    sleep
-    sublw file
-    subwf file,1
-    subwfb file,1
-    swapf file,1
-    tris file
-    xorlw file
-    xorwf file,1
+        nop
+        option
+        reset
+        retfie
+        retlw  file
+        return
+        rlf    file,1
+        rrf    file,1
+        sleep
+        sublw  file
+        subwf  file,1
+        subwfb file,1
+        swapf  file,1
+        tris   file
+        xorlw  file
+        xorwf  file,1
 
-    end
+; HIGH
+        data  HIGH table_code
+        data  HIGH table_data
+
+table_code
+
+        udata
+table_data res 1
+        end
