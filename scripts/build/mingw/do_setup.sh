a4654dd502a03e532a7979bbf14c567027f730ca
borutr <borutr@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2013-08-09 12:19:08 +0000

    * build/mingw/do_setup.sh, build/mingw/make-mingw:   revived the Linux cross compilation build

--- scripts/build/mingw/do_setup.sh
+++ scripts/build/mingw/do_setup.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
 
-if test -n "$1"
+# do_setup.sh - front end for the make-mingw script
+#
+# Copyright (c) 2009-2013 Borut Razem
+#
+# This file is part of gputils.
+#
+#  This software is provided 'as-is', without any express or implied
+#  warranty.  In no event will the authors be held liable for any damages
+#  arising from the use of this software.
+#
+#  Permission is granted to anyone to use this software for any purpose,
+#  including commercial applications, and to alter it and redistribute it
+#  freely, subject to the following restrictions:
+#
+#  1. The origin of this software must not be misrepresented; you must not
+#     claim that you wrote the original software. If you use this software
+#     in a product, an acknowledgment in the product documentation would be
+#     appreciated but is not required.
+#  2. Altered source versions must be plainly marked as such, and must not be
+#     misrepresented as being the original software.
+#  3. This notice may not be removed or altered from any source distribution.
+#
+#  Borut Razem
+#  borut.razem@users.sourceforge.net
+
+if [ -n "$1" ]
 then
   srcfilename=$1
 else
@@ -8,23 +33,18 @@ else
   srcfilename=gputils-${ver}.tar.gz
 fi
 
-if test -n "$2"
+if [ -n "$2" ]
 then
   packager=$2
 else
   packager="Borut Razem"
 fi
 
-if test -n "$3"
+if [ -n "$3" ]
 then
   email=$3
 else
   email=borut.razem@siol.net
 fi
 
-# gcc-3
-#CC="gcc -mno-cygwin" ./make-mingw --email=$email --name="$packager" --source=$srcfilename --save-temps
-#gcc-4
-CC="i686-pc-mingw32-gcc" ./make-mingw --email=$email --name="$packager" --source=$srcfilename --save-temps
-# on mingw32:
-#CC=gcc ./make-mingw --email=$email --name="$packager" --source=$srcfilename --save-temps
+./make-mingw --email=$email --name="$packager" --source=$srcfilename --save-temps
