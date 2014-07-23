0ef6f273e943b6dd092803f941de403d1b65e14e
molnarkaroly <molnarkaroly@2421ac41-8990-4fbb-a74b-d7adc9fc73e3>
2014-07-23 08:30:58 +0000

    * gputils/include/stdhdr.h, gputils/libgputils/gpdis.c:   Applied the patch #63:   "GCC diagnostic pragma patch for compilers older than gcc 4.6"   Thanks to Vikas N Kumar.

--- gputils/include/stdhdr.h
+++ gputils/include/stdhdr.h
@@ -91,4 +91,30 @@ char *strndup (const char *s, size_t n);
 
 #define GPUTILS_COPYRIGHT_STRING "Copyright (c) 1998-2014 gputils project"
 
+#if ((__GNUC__ * 100) + __GNUC_MINOR__) >= 402
+  #define GPUTILS_GCC_STR(s)            #s
+  #define GPUTILS_GCC_JOINSTR(x,y)      GPUTILS_GCC_STR(x ## y)
+  #define GPUTILS_GCC_DO_PRAGMA(x)      _Pragma(#x)
+  #define GPUTILS_GCC_PRAGMA(x)         GPUTILS_GCC_DO_PRAGMA(GCC diagnostic x)
+
+  #if ((__GNUC__ * 100) + __GNUC_MINOR__) < 406
+    /* needed for older GCCs that accept only top of the file diagnostic pragmas */
+    #define GPUTILS_GCC_DIAG_TOP(x)     GPUTILS_GCC_PRAGMA(ignored GPUTILS_GCC_JOINSTR(-W,x))
+    #define GPUTILS_GCC_DIAG_OFF(x)
+    #define GPUTILS_GCC_DIAG_ON(x)
+  #else
+    #define GPUTILS_GCC_DIAG_TOP(x)
+
+    #define GPUTILS_GCC_DIAG_OFF(x) \
+                GPUTILS_GCC_PRAGMA(push) \
+                GPUTILS_GCC_PRAGMA(ignored GPUTILS_GCC_JOINSTR(-W,x))
+
+    #define GPUTILS_GCC_DIAG_ON(x)      GPUTILS_GCC_PRAGMA(pop)
+  #endif
+#else
+  #define GPUTILS_GCC_DIAG_TOP(x)
+  #define GPUTILS_GCC_DIAG_OFF(x)
+  #define GPUTILS_GCC_DIAG_ON(x)
 #endif
+
+#endif /* __STDHDR_H__ */
